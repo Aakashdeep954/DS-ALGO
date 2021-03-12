f676252879e439755b218d58aaa0800f7467e7c1
Aakashdeep954 <aakashdeep954@gmail.com>
2021-03-13 04:20:29 +0530

    Create n_queen.cpp

---
+++ coding ninjas/competetive_programming/back_tracking/n_queen.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i, c) for (int(i) = 0; (i) < (c); (i)++)
+#define go(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define gc getchar_unlocked
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+
+//---------------------------------------pair----------------------------------------------------//
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<int, string> pistr;
+//---------------------------------------pair----------------------------------------------------//
+
+//--------------------------------------vector---------------------------------------------------//
+typedef vector<ll> vl;
+typedef vector<pll> vpll;
+typedef vector<vl> vvl;
+
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+
+typedef vector<string> vstr;
+typedef vector<pistr> vpistr;
+typedef vector<vstr> vvstr;
+//---------------------------------------vector---------------------------------------------------//
+
+//------------------------------------unordered_map-----------------------------------------------//
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<ll, ll> umapll;
+typedef unordered_map<int, vi> umapivi;
+
+typedef unordered_map<string, int> umapstri;
+typedef unordered_map<int, string> umapistr;
+//------------------------------------unordered_map-----------------------------------------------//
+
+//----------------------------------------map-----------------------------------------------------//
+typedef map<int, int> mapii;
+typedef map<ll, ll> mapll;
+//----------------------------------------map-----------------------------------------------------//
+
+//----------------------------------------set-----------------------------------------------------//
+typedef set<int> sei;
+typedef set<ll> sel;
+//----------------------------------------set-----------------------------------------------------//
+
+//----------------------------------execution time------------------------------------------------//
+#define EXT cout << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl
+//----------------------------------execution time------------------------------------------------//
+
+bool is_safe(int **arr, int x, int y, int n)
+{
+    fo(row, x)
+    {
+        if (arr[row][y] == 1)
+        {
+            return false;
+        }
+    }
+    int row = x;
+    int col = y;
+    while (row >= 0 && col >= 0)
+    {
+        if (arr[row][col] == 1)
+        {
+            return false;
+        }
+        row--;
+        col--;
+    }
+    row = x;
+    col = y;
+    while (row >= 0 && col < n)
+    {
+        if (arr[row][col] == 1)
+        {
+            return false;
+        }
+        row--;
+        col++;
+    }
+    return true;
+}
+
+void n_queen(int **arr, int x, int n)
+{
+    if (x == n)
+    {
+         fo(row, n)
+        {
+            fo(col, n)
+            {
+                cout << arr[row][col] << " ";
+            }
+        }
+        cout<<endl;
+    }
+    fo(col, n)
+    {
+        if (is_safe(arr, x, col, n))
+        {
+            arr[x][col] = 1;
+            n_queen(arr, x + 1, n);
+            arr[x][col] = 0;
+        }
+    }
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    int **arr = new int *[n];
+    fo(i, n)
+    {
+        arr[i] = new int[n];
+        fo(j, n)
+        {
+            arr[i][j] = 0;
+        }
+    }
+    n_queen(arr, 0, n);
+   
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
