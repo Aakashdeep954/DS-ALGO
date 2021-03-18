c89cadd20ae26f6a462d355c1f57235db257b881
Aakashdeep954 <aakashdeep954@gmail.com>
2021-03-18 23:36:01 +0530

    Add files via upload

---
+++ coding ninjas/data_structures/recursion_1/last_index_of_number.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i,c) for(int(i) = 0; (i) < (c); (i)++)
+#define gc getchar_unlocked
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+#define EXT cout << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl
+
+int lastIndex(int arr[],int size,int x){
+    if(size==0){
+        return -1;
+    }
+    int output = lastIndex(arr+1,size-1,x);
+    if(output==-1){
+        if(arr[0]==x){
+            return 0;
+        }
+    }
+    else{
+        return output+1;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+    int n;
+    cin>>n;
+    int arr[n];
+    fo(i,n){
+        cin>>arr[i];
+    }
+    int x;
+    cin>>x;
+    cout<<lastIndex(arr,n,x);
+}
