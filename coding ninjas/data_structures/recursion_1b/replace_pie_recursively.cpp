1c5503cdfabc56109c0cdd53fe2571f0ebfd7f01
Aakashdeep954 <aakashdeep954@gmail.com>
2021-03-19 03:13:54 +0530

    Create replace_pie_recursively.cpp

---
+++ coding ninjas/data_structures/recursion_1b/replace_pie_recursively.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i, c) for (int(i) = 0; (i) < (c); (i)++)
+#define go(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define gc getchar_unlocked
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+
+//---------------------------------------pair----------------------------------------------------//
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<int, string> pistr;
+//---------------------------------------pair----------------------------------------------------//
+
+//--------------------------------------vector---------------------------------------------------//
+typedef vector<ll> vl;
+typedef vector<pll> vpll;
+typedef vector<vl> vvl;
+
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+
+typedef vector<string> vstr;
+typedef vector<pistr> vpistr;
+typedef vector<vstr> vvstr;
+//---------------------------------------vector---------------------------------------------------//
+
+//------------------------------------unordered_map-----------------------------------------------//
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<ll, ll> umapll;
+typedef unordered_map<int, vi> umapivi;
+
+typedef unordered_map<string, int> umapstri;
+typedef unordered_map<int, string> umapistr;
+//------------------------------------unordered_map-----------------------------------------------//
+
+//----------------------------------------map-----------------------------------------------------//
+typedef map<int, int> mapii;
+typedef map<ll, ll> mapll;
+//----------------------------------------map-----------------------------------------------------//
+
+//----------------------------------------set-----------------------------------------------------//
+typedef set<int> sei;
+typedef set<ll> sel;
+//----------------------------------------set-----------------------------------------------------//
+
+//----------------------------------execution time------------------------------------------------//
+#define EXT cout << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl
+//----------------------------------execution time------------------------------------------------//
+
+void replace_pie(char input[], char pie[])
+{
+    if (input[1] == '\0')
+    {
+        return;
+    }
+    if (input[0] == 'p' && input[1] == 'i')
+    {
+        go(i, strlen(input), 1)
+        {
+            input[i + 2] = input[i];
+        }
+        input[0] = pie[0];
+        input[1] = pie[1];
+        input[2] = pie[2];
+        input[3] = pie[3];
+    }
+
+    replace_pie(input + 1, pie);
+}
+void solve()
+{
+    char input[10000];
+    cin >> input;
+    char pie[] = {'3', '.', '1', '4'};
+    replace_pie(input, pie);
+    cout << input << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
