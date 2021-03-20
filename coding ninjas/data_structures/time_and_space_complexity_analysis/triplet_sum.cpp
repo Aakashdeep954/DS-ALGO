10743f5686820127fcc8d209e0f3e692dd9d6b03
Aakashdeep954 <aakashdeep954@gmail.com>
2021-03-21 04:06:52 +0530

    Update triplet_sum.cpp

--- coding ninjas/data_structures/time_and_space_complexity_analysis/triplet_sum.cpp
+++ coding ninjas/data_structures/time_and_space_complexity_analysis/triplet_sum.cpp
@@ -15,7 +15,51 @@ typedef vector<ll> vi;
 typedef vector<pii> vpii;
 typedef vector<vi> vvi;
 #define EXT cout << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl
+int pair_sum(int arr[], int start, int n, int x)
+{
+    int start_pointer = start;
+    int end_pointer = n - 1;
+    int count = 0;
+    while (start_pointer < end_pointer)
+    {
+        if (arr[start_pointer] + arr[end_pointer] < x)
+        {
+            start_pointer++;
+        }
+        else if (arr[start_pointer] + arr[end_pointer] > x)
+        {
+            end_pointer--;
+        }
+        else
+        {
+            int l = arr[start_pointer];
+            int r = arr[end_pointer];
+            if (l == r)
+            {
+                int total_elements = end_pointer - start_pointer + 1;
+                count += ((total_elements - 1) * (total_elements) / 2);
+                return count;
+            }
+            int temp_start_pointer = start_pointer + 1;
+            int temp_end_pointer = end_pointer - 1;
+            while (temp_start_pointer <= temp_end_pointer && arr[temp_start_pointer] == l)
+            {
+                temp_start_pointer++;
+            }
+            while (temp_end_pointer >= temp_start_pointer && arr[temp_end_pointer] == r)
+            {
+                temp_end_pointer--;
+            }
+            int total_elements_from_start = temp_start_pointer - start_pointer;
+            int total_elements_from_end = end_pointer - temp_end_pointer;
+            count += (total_elements_from_start) * (total_elements_from_end);
+            start_pointer = temp_start_pointer;
+            end_pointer = temp_end_pointer;
+        }
+    }
 
+    return count;
+}
 void solve()
 {
     int n;
@@ -27,21 +71,13 @@ void solve()
     }
     int x;
     cin >> x;
-    int count = 0;
-    fo(i, n - 1)
+    int ans = 0;
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i++)
     {
-        unordered_map<int, int> m;
-        go(j, i + 1, n)
-        {
-            int sum = arr[i] + arr[j];
-            if (m.find(x - sum) != m.end())
-            {
-                count += m[x - sum];
-            }
-            m[arr[j]]++;
-        }
+        ans += pair_sum(arr, i + 1, n, x - arr[i]);
     }
-    cout << count;
+    cout << ans << endl;
 }
 int main()
 {
