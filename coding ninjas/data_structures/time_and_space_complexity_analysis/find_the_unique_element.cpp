9df6e075a8b16e487a7f8cba2eccc73cfcf81829
Aakashdeep954 <aakashdeep954@gmail.com>
2021-03-21 00:39:03 +0530

    Create find_the_unique_element.cpp

---
+++ coding ninjas/data_structures/time_and_space_complexity_analysis/find_the_unique_element.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i,c) for(int(i) = 0; (i) < (c); (i)++)
+#define go(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define gc getchar_unlocked
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+#define EXT cout << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl
+
+void solve(){
+    int n;
+    cin>>n;
+    int arr[n];
+    fo(i,n){
+        cin>>arr[i];
+    }
+    int xorr=arr[0];
+    go(i,1,n){
+        xorr^=arr[i];
+    }
+    cout<<xorr<<endl;
+
+}
+int main()
+{
+    ios::sync_with_stdio(0); 
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
