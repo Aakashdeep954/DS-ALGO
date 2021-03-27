d52308399721b9e45648f6f83dd950fb51ef996d
Aakashdeep954 <aakashdeep954@gmail.com>
2021-03-28 01:47:30 +0530

    Add files via upload

---
+++ coding ninjas/data_structures/stacks_and_queues/stock_span.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fo(i, c) for (int(i) = 0; (i) < (c); (i)++)
+#define go(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define gc getchar_unlocked
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+
+//---------------------------------------pair----------------------------------------------------//
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef pair<int, string> pistr;
+//---------------------------------------pair----------------------------------------------------//
+
+//--------------------------------------vector---------------------------------------------------//
+typedef vector<ll> vl;
+typedef vector<pll> vpll;
+typedef vector<vl> vvl;
+
+typedef vector<int> vi;
+typedef vector<pii> vpii;
+typedef vector<vi> vvi;
+
+typedef vector<string> vstr;
+typedef vector<pistr> vpistr;
+typedef vector<vstr> vvstr;
+//---------------------------------------vector---------------------------------------------------//
+
+//------------------------------------unordered_map-----------------------------------------------//
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<ll, ll> umapll;
+typedef unordered_map<int, vi> umapivi;
+
+typedef unordered_map<string, int> umapstri;
+typedef unordered_map<int, string> umapistr;
+//------------------------------------unordered_map-----------------------------------------------//
+
+//----------------------------------------map-----------------------------------------------------//
+typedef map<int, int> mapii;
+typedef map<ll, ll> mapll;
+//----------------------------------------map-----------------------------------------------------//
+
+//----------------------------------------set-----------------------------------------------------//
+typedef set<int> sei;
+typedef set<ll> sel;
+//----------------------------------------set-----------------------------------------------------//
+
+//----------------------------------execution time------------------------------------------------//
+#define EXT cout << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl
+//----------------------------------execution time------------------------------------------------//
+int *stock_span(int arr[], int n)
+{
+    int *output = new int[n];
+    stack<int> st;
+    st.push(0);
+    output[0] = 1;
+    go(i, 1, n)
+    {
+        if (arr[i] > arr[i - 1])
+        {
+            st.pop();
+            while (!st.empty() && arr[i] > arr[st.top()])
+            {
+                st.pop();
+            }
+            if (st.empty())
+            {
+                output[i] = i + 1;
+            }
+            else
+            {
+                output[i] = i - st.top();
+            }
+        }
+        else
+        {
+            output[i] = 1;
+        }
+        st.push(i);
+    }
+    return output;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    fo(i, n)
+    {
+        cin >> arr[i];
+    }
+    int *ans = stock_span(arr, n);
+    fo(i, n)
+    {
+        cout << ans[i] << " ";
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
